a3b448de6d82fe7542587361d000ca3ef1097a9c
Hayoon <hy97@sju.ac.kr>
2022-09-24 15:43:12 +0900

    0924_hayoon

--- src/main/java/mbti/mbti_test/config/security/WebConfig.java
+++ src/main/java/mbti/mbti_test/config/security/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://43.200.94.144:3000")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true)
